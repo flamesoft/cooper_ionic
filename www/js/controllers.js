a0f2f4d2d7b2c0eb484b1c5b2699e1b64e8fa277
Yan Zhang <emma2740@gmail.com>
2016-06-14 14:31:28 +0200

    WIP-add save button Still the auth part is to be done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -81,9 +81,15 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PerformanceCtrl', function($scope, performaceData){
-  $scope.saveData = function(){
-
+  $scope.saveData = function(person){
+    data = {performace_data: {data: {message: person.message}}}
+    performaceData.save(data, function(response){
+        console.log(response);
+      }, function(error){
+        console.log(error);
+      })
   };
+
   $scope.retrieveData = function(){
 
   };
